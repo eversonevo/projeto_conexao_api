7d0505a1b03a9c4cb02afae8f6e941e0185caebc
eversonevo <evotreinamentos@gmail.com>
2024-05-21 13:30:05 -0300

    subindo

--- controllers/teste_controller.js
+++ controllers/teste_controller.js
@@ -56,7 +56,7 @@ exports.insereDados = async (req, res, next) => {
         const response = {
             mensagem: 'Dado inserido com sucesso!',
             dadoCriado: {
-                id: result.id,
+                id: result.insertId,
                 nome: req.body.nome,
                 telefone: req.body.telefone,
                 request: {
@@ -73,31 +73,90 @@ exports.insereDados = async (req, res, next) => {
     }
 }
 
-/*
+// ALTERA PRODUTO - USANDO ASYNC / AWAIT - FUNCTION SÓ PARA ACESSAR MYSQL
+exports.alteraDado = async (req, res, next) => {
+
+    try {
+        const query = "UPDATE teste SET nome = ?, telefone = ? WHERE id = ?";
+        await mysql.execute(query,[
+            req.body.nome, 
+            req.body.telefone, 
+            req.body.id,
+        ]);
+        const response = {
+            mensagem: 'Produto alterado com sucesso!',
+            dadoAtualizado: {
+                id: req.body.id,
+                nome: req.body.nome,
+                telefone: req.body.telefone,
+                request: {
+                    tipo: 'GET',
+                    description: 'Retorna detalhes de um produto',
+                    url: process.env.URL_API + 'dados/' + req.body.id
+                }
+            }
+        }
+
+        return res.status(202).send(response);
+
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+}
+
+// *********************************************************************************************
+
+// REMOVE PRODUTO - USANDO ASYNC / AWAIT - FUNCTION SÓ PARA ACESSAR MYSQL
+exports.removeDado = async (req, res, next) => {
+
+    try {
+        const query = "DELETE FROM teste WHERE id = ?;";
+        const result = await mysql.execute(query,[req.body.id]);
+        const response = {
+            mensagem: 'Produto removido com sucesso!',
+            request: {
+                tipo: 'POST',
+                description: 'Insere um produto',
+                url: process.env.URL_API + 'dados/',
+                body: {
+                    nome: 'String',
+                    telefone: 'String'
+                }
+            }
+    
+        }
+    
+        res.status(202).send(response);
+    } catch (error) {
+        return res.status(500).send({ error: error });
+    }
+
+}
 
 
 // *********************************************************************************************
 
+
 // CONSULTA UM PRODUTO - USANDO ASYNC / AWAIT - FUNCTION SÓ PARA ACESSAR MYSQL
-exports.getIdProduto = async (req, res, next) => {
+exports.getIdDado = async (req, res, next) => {
     
     try {
         console.log("oi");
 
-        const query = "SELECT * FROM produtos WHERE id_produto = ?;";
-        const result = await mysql.execute(query,[req.params.id_produto]);
+        const query = "SELECT * FROM teste WHERE id = ?;";
+        const result = await mysql.execute(query,[req.body.id]);
         console.log(result.length);
         if (result.length === 0) {
             return res.status(404).send({
-                mensagem: "Não foi encontrado nenhum produto com este ID"
+                mensagem: "Não foi encontrado nenhum dado com este ID"
             });
         }
         const response = {
-            produto: {
-                id_produto: result[0].id_produto,
+            dado: {
+                id: result[0].id,
                 nome: result[0].nome,
-                preco: result[0].preco,
-                imagem_produto: process.env.URL_IMAGE_PROD+result[0].imagem_produto,
+                telefone: result[0].telefone,
+                //imagem_produto: process.env.URL_IMAGE_PROD+result[0].imagem_produto,
                 request: {
                     tipo: 'GET',
                     description: 'Retorna um produto',
@@ -114,35 +173,40 @@ exports.getIdProduto = async (req, res, next) => {
     }
 }
 
-
 // *********************************************************************************************
 
-// ALTERA PRODUTO - USANDO ASYNC / AWAIT - FUNCTION SÓ PARA ACESSAR MYSQL
-exports.updateProduto = async (req, res, next) => {
 
+// CONSULTA UM PRODUTO - USANDO ASYNC / AWAIT - FUNCTION SÓ PARA ACESSAR MYSQL
+exports.getNomeDado = async (req, res, next) => {
+    
     try {
-        const query = "UPDATE produtos SET nome = ?, preco = ? WHERE id_produto = ?";
-        await mysql.execute(query,[
-            req.body.nome, 
-            req.body.preco, 
-            req.body.id_produto,
-        ]);
+        console.log("oi");
+
+        const query = "SELECT * FROM teste WHERE nome = ?;";
+        const result = await mysql.execute(query,[req.body.nome]);
+        console.log(result.length);
+        if (result.length === 0) {
+            return res.status(404).send({
+                mensagem: "Não foi encontrado nenhum dado com este ID"
+            });
+        }
         const response = {
-            mensagem: 'Produto alterado com sucesso!',
-            produtoAtualizado: {
-                id_produto: req.body.id_produto,
-                nome: req.body.nome,
-                preco: req.body.preco,
+            dado: {
+                id: result[0].id,
+                nome: result[0].nome,
+                telefone: result[0].telefone,
+                //imagem_produto: process.env.URL_IMAGE_PROD+result[0].imagem_produto,
                 request: {
                     tipo: 'GET',
-                    description: 'Retorna detalhes de um produto',
-                    url: process.env.URL_API + 'produtos/' + req.body.id_produto
+                    description: 'Retorna um produto',
+                    url: process.env.URL_API + 'produtos'
                 }
             }
         }
 
-        return res.status(202).send(response);
+        return res.status(200).send(response);
 
+        
     } catch (error) {
         return res.status(500).send({ error: error });
     }
@@ -150,30 +214,3 @@ exports.updateProduto = async (req, res, next) => {
 
 // *********************************************************************************************
 
-// REMOVE PRODUTO - USANDO ASYNC / AWAIT - FUNCTION SÓ PARA ACESSAR MYSQL
-exports.removeProduto = async (req, res, next) => {
-
-    try {
-        const query = "DELETE FROM produtos WHERE id_produto = ?;";
-        const result = await mysql.execute(query,[req.body.id_produto]);
-        const response = {
-            mensagem: 'Produto removido com sucesso!',
-            request: {
-                tipo: 'POST',
-                description: 'Insere um produto',
-                url: process.env.URL_API + 'produtos/',
-                body: {
-                    nome: 'String',
-                    preco: 'double number'
-                }
-            }
-    
-        }
-    
-        res.status(202).send(response);
-    } catch (error) {
-        return res.status(500).send({ error: error });
-    }
-
-}
-*/
